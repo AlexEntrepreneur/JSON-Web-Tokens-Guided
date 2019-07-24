78e924db660092345d46400eea8ccf4311e9494c
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-07-24 08:45:29 +0100

    Edit Project Files

--- users/users-router.js
+++ users/users-router.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 
 const Users = require('./users-model.js');
-const restricted = require('../auth/restricted-middleware.js');
-const checkRole = require('../auth/check-role-middleware.js');
 
-// router.get('/', restricted, (req, res) => {
-router.get('/', restricted, checkRole('Student'), (req, res) => {
+router.get('/', (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
