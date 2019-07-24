d39ff0614eec04f47657cc57842cee2e825bb76a
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-07-24 09:53:18 +0100

    .

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); // import jwt
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +40,19 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+    // Some other data here
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, 'THIS IS A REALLY LONG SECRET', options);
+}
 
 module.exports = router;
