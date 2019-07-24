0da472a1370faef6a09456a3c708a79c962100a7
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-07-24 10:07:47 +0100

    .

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,1 +1,18 @@
-// Middleware goes here
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const authHeaderIsPresent = req.authorization;
+
+    if (authHeaderIsPresent) {
+        jwt.verify(authHeaderIsPresent, 'THIS IS A REALLY LONG SECRET', (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ you: 'can\'t touch this' });
+            } else {
+                req.decodedToken = decodedToken;
+                console.log('decoded token', req.decodedToken);
+                
+                next();
+            }
+        });
+    }
+};
